67923959e5e581acb133abc01453524d2b17362b
Student <example@example.com>
2019-11-21 10:43:45 -0800

    restructure script files, fix error try/catch, and convert functions to async

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { Robogotchi } from './scripts/robo.js';
-import { DisplayService } from './scripts/display-service.js';
 import $ from 'jquery';
 import './styles.css';
 import staticGIF from './assets/static.gif';
@@ -8,18 +7,18 @@ import staticGIF from './assets/static.gif';
 $(document).ready(function () {
     $('#face').attr('src', staticGIF);
     $('#statusBar').hide();
-    let display = new DisplayService();
+
     let robo;
     $('input#name').on('change', function () {
         robo = new Robogotchi($('input#name').val());
-        display.start(robo);
         $('#statusBar').fadeIn();
     });
+
     $('input#searchbar').on('change', function () {
         let searchTerm = $('#searchbar').val();
-        robo.movieTitleSearch(searchTerm);
-        // display.displayMovieTitle(movieTitleResult);
-        // $('#movieTitle').html(movieTitleResult);
-        // console.log(search);
-    })
+        const movieTitleResult = robo.movieTitleSearch(searchTerm);
+        robo.display.MovieTitle(robo, movieTitleResult);
+        $('#movieTitle').html(movieTitleResult);
+    });
+
 });
